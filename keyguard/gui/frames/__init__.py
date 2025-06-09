48677731a3c1744118be17be23331637b2610927
Bajraktar zi Lvovu <39407654+looomenn@users.noreply.github.com>
2025-06-09 19:44:52 +0300

    refactor: code refactoring frames/__init__.py

--- keyguard/gui/frames/__init__.py
+++ keyguard/gui/frames/__init__.py
@@ -1,8 +1,9 @@
 """Frames package for KeyGuard application."""
 
-from .MainFrame import MainFrame
+from .auth_frame import AuthFrame
+from .main_frame import MainFrame
 
 __all__ = [
-    'MainFrame',
+    "AuthFrame",
+    "MainFrame",
 ]
-
